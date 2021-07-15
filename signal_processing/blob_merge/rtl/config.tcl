67aca2e3a1ed70b6c86c868a5bd8078fdc28a1cc
komaljaved-lm <komal.javed@lampromellon.com>
2021-07-15 09:55:30 +0500

    added config files

---
+++ signal_processing/blob_merge/rtl/config.tcl
@@ -0,0 +1,4 @@
+set ::env(PROJECT_NAME) blob_merge
+set ::env(DESIGN_TOP) RLE_BlobMerging
+set ::env(DESIGN_DIR) "RTL_Benchmark/signal_processing/blob_merge/rtl"
+set ::env(TOP_VERILOG) "RTL_Benchmark/signal_processing/blob_merge/rtl/blob_merge.v"
