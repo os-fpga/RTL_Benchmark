800b8b3f7b2ea0b8811c05f1c9807d430b44f7b8
Aashir Ahsan <aashir.ahsan@ithelp@lampromellon.com>
2021-07-29 16:44:08 +0500

    Benchmarks added

---
+++ controller/usb1.1_phy/rtl/config.tcl
@@ -0,0 +1,4 @@
+set ::env(PROJECT_NAME) usb1.1_phy
+set ::env(DESIGN_TOP) usb_phy_top
+set ::env(DESIGN_DIR) ./controller/usb1.1_phy/rtl
+set ::env(TOP_VERILOG) ./controller/usb1.1_phy/rtl/usb_phy_top.v
