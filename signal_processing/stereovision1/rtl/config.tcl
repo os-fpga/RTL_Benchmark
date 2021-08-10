9654cf76d4815b5ce59a3ddeaab18c7c8f30f582
laraibkhan-rs <laraib.khan@rapidsilicon.com>
2021-08-10 09:52:58 +0500

    renamed stereovision1

---
+++ signal_processing/stereovision1/rtl/config.tcl
@@ -0,0 +1,5 @@
+set ::env(PROJECT_NAME) stereovision1
+set ::env(DESIGN_TOP) sv_chip1_hierarchy_no_mem
+set ::env(DESIGN_DIR) RTL_Benchmark/signal_processing/stereovision1/rtl
+set ::env(TOP_VERILOG) RTL_Benchmark/signal_processing/stereovision1/rtl/stereovision1.v
+set ::env(CLOCK_PORT) tm3_clk_v0
--- signal_processing/stereovission1/rtl/config.tcl
+++
@@ -1,5 +0,0 @@
-set ::env(PROJECT_NAME) stereovission1
-set ::env(DESIGN_TOP) sv_chip1_hierarchy_no_mem
-set ::env(DESIGN_DIR) RTL_Benchmark/signal_processing/stereovission1/rtl
-set ::env(TOP_VERILOG) RTL_Benchmark/signal_processing/stereovission1/rtl/stereovision1.v
-set ::env(CLOCK_PORT) tm3_clk_v0
