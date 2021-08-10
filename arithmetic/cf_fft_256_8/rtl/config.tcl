6bc5ee31ee552575cb943e8a3f99d95aad1d9869
laraibkhan-rs <laraib.khan@rapidsilicon.com>
2021-08-10 09:50:00 +0500

    fft changes to fast_fourier_transform

--- arithmetic/cf_fft_256_8/rtl/config.tcl
+++
@@ -1,5 +0,0 @@
-set ::env(PROJECT_NAME) cf_fft_256_8
-set ::env(DESIGN_TOP) cf_fft_256_8
-set ::env(DESIGN_DIR) "RTL_Benchmark/arithmetic/cf_fft_256_8/rtl"
-set ::env(TOP_VERILOG) "RTL_Benchmark/arithmetic/cf_fft_256_8/rtl/cf_fft_256_8.v"
-set ::env(CLOCK_PORT) clock_c
