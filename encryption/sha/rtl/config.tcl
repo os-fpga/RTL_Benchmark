9f8d37fbdbf6b2e73cd47f2f2ba3d123a990b9dd
Aashir Ahsan <aashir.ahsan@ithelp@lampromellon.com>
2021-08-17 12:28:10 +0500

    sha removed

--- encryption/sha/rtl/config.tcl
+++
@@ -1,6 +0,0 @@
-set ::env(PROJECT_NAME) sha
-set ::env(DESIGN_TOP) sha
-set ::env(DESIGN_DIR) RTL_Benchmark/encryption/sha/rtl
-set ::env(TOP_VERILOG) RTL_Benchmark/encryption/sha/rtl/sha.v
-set ::env(CLOCK_PORT) clk_i
-set ::env(DOMAIN) encryption
