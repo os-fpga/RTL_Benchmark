c6bb04c2783fac953f1647f3e17686b2f47a2491
Aashir Ahsan <aashir.ahsan@ithelp@lampromellon.com>
2021-08-25 10:18:52 +0500

    des3 removed

--- encryption/des3/rtl/verilog/area_opt/config.tcl
+++
@@ -1,6 +0,0 @@
-set ::env(PROJECT_NAME) des3_ao
-set ::env(DESIGN_TOP) des3_top
-set ::env(DESIGN_DIR) RTL_Benchmark/encryption/des3/rtl/verilog/area_opt
-set ::env(TOP_VERILOG) RTL_Benchmark/encryption/des3/rtl/verilog/area_opt/des3_top.v
-set ::env(CLOCK_PORT) clk
-set ::env(DOMAIN) encryption
--- encryption/des3/rtl/verilog/perf_opt/config.tcl
+++
@@ -1,6 +0,0 @@
-set ::env(PROJECT_NAME) des3_po
-set ::env(DESIGN_TOP) des3
-set ::env(DESIGN_DIR) RTL_Benchmark/encryption/des3/rtl/verilog/perf_opt
-set ::env(TOP_VERILOG) RTL_Benchmark/encryption/des3/rtl/verilog/perf_opt/des3_top.v
-set ::env(CLOCK_PORT) clk
-set ::env(DOMAIN) encryption
