47af7b19a85d0b9c8e711cac7621efdb2bb04e7a
aashir.ahsan-rs <aashir.ahsan@rapidsilicon.com>
2021-09-16 16:16:13 +0500

    micro benchmarks added

--- digital_design/counter120bitx5/rtl/config.tcl
+++
@@ -1,6 +0,0 @@
-set ::env(PROJECT_NAME) counter120bitx5
-set ::env(DESIGN_TOP) counter120bitx5
-set ::env(DESIGN_DIR) RTL_Benchmark/digital_design/counter120bitx5/rtl
-set ::env(TOP_VERILOG) RTL_Benchmark/digital_design/counter120bitx5/rtl/counter_5_120_13.v
-set ::env(CLOCK_PORT) "clk1, clk2, clk3, clk4, clk5"
-set ::env(DOMAIN) digital_design
