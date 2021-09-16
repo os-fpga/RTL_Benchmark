a52b2b5dd39207606d6a8020a52643f8cc66ad72
aashir.ahsan-rs <aashir.ahsan@rapidsilicon.com>
2021-09-16 15:57:39 +0500

    config files updated

--- controller/usb2.0/rtl/verilog/config.tcl
+++
@@ -1,6 +0,0 @@
-set ::env(PROJECT_NAME) usb2.0
-set ::env(DESIGN_TOP) usbf_top
-set ::env(DESIGN_DIR) RTL_Benchmark/controller/usb2.0/rtl/verilog
-set ::env(TOP_VERILOG) RTL_Benchmark/controller/usb2.0/rtl/verilog/usb2.0.v
-set ::env(CLOCK_PORT) clk_i
-set ::env(DOMAIN) controller
--- digital_design/counter/rtl/config.tcl
+++ digital_design/counter/rtl/config.tcl
@@ -3,4 +3,4 @@ set ::env(DESIGN_TOP) counter
 set ::env(DESIGN_DIR) RTL_Benchmark/digital_design/counter/rtl
 set ::env(TOP_VERILOG) RTL_Benchmark/digital_design/counter/rtl/counter.v
 set ::env(CLOCK_PORT) clk_counter
-set ::env(DOMAIN) digital_deisgn
+set ::env(DOMAIN) digital_design
