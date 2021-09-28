e1737f667eeaf69ec176cd20908df40ea5c2481c
komaljaved-rs <komal.javed@rapidsilicon.com>
2021-09-28 10:22:23 +0500

    removed cga_test branch from ci tests

--- .github/scripts/tools_auto_scripts/vivado/viv_script.tcl
+++ .github/scripts/tools_auto_scripts/vivado/viv_script.tcl
@@ -3,10 +3,10 @@ create_project $::env(PROJECT_NAME) $::env(CGA_ROOT)/misc/vivado/$::env(PROJECT_
 add_files -norecurse $::env(CGA_ROOT)/../$::env(DESIGN_DIR)
 
 if {[glob -nocomplain -directory "$::env(CGA_ROOT)/../$::env(DESIGN_DIR)" "*.vh"] != ""} {
-  set_property is_global_include true [get_files [glob -nocomplain -directory "$::env(CGA_ROOT)/$::env(DESIGN_DIR)" "*.vh"]] 
+  set_property is_global_include true [get_files [glob -nocomplain -directory "$::env(CGA_ROOT)/../$::env(DESIGN_DIR)" "*.vh"]] 
 }
 if {[glob -nocomplain -directory "$::env(CGA_ROOT)/../$::env(DESIGN_DIR)" "*.svh"] != ""} {
-  set_property is_global_include true [get_files [glob -nocomplain -directory "$::env(CGA_ROOT)/$::env(DESIGN_DIR)" "*.svh"]] 
+  set_property is_global_include true [get_files [glob -nocomplain -directory "$::env(CGA_ROOT)/../$::env(DESIGN_DIR)" "*.svh"]] 
 }
 
 set_property SEVERITY {Warning} [get_drc_checks NSTD-1]
