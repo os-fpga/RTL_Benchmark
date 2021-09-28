8a1d8e984ecb08dc2a30d5ffbeee6c8039a28a12
Laraib Khan <87127798+laraibkhan-rs@users.noreply.github.com>
2021-09-28 10:51:30 +0500

    int i changes to integer i

--- scalable_counters/johnson_counter/rtl/johnson_counter.v
+++ scalable_counters/johnson_counter/rtl/johnson_counter.v
@@ -2,9 +2,8 @@
 //  Functionality: An n-bit johnson counter 
 //  Author:        Laraib Khan
 //-------------------------------------------------------
-
 `timescale 1ns / 1ps
-`define size 5
+`define size 298
 
 module johnson_counter
   (  
@@ -12,13 +11,13 @@ module johnson_counter
 	input rstn,
   	output reg [`size-1:0] out
   );    
- 
+  integer i;
   always @ (posedge clk) begin
       if (!rstn)
          out <= 1;
       else begin
         out[`size-1] <= ~out[0];
-        for (int i = 0; i < `size-1; i=i+1) begin
+        for ( i = 0; i < `size-1; i=i+1) begin
           out[i] <= out[i+1];
         end
       end
