5f537bfa4d019cf35d46c1c2cf78b41a9cbab4f6
komaljaved-rs <komal.javed@rapidsilicon.com>
2021-09-28 10:26:01 +0500

    modified ripple counter

--- scalable_counters/ripple_counter/rtl/ripple_counter.v
+++ scalable_counters/ripple_counter/rtl/ripple_counter.v
@@ -12,20 +12,20 @@ module ripple_counter (q, clk, reset);
   output [`size-1:0] q;
   input clk, reset;
 
-tff tff0(q[0], ~clk, reset);
+dut_ff dut_ff0(q[0], ~clk, reset);
 
 genvar i;	
 	// Generate for loop to instantiate N times
 	generate 
 		for (i = 1; i < `size; i = i + 1) begin
-          tff tn (.q(q[i]),.clk(~q[i-1]),.reset(reset));
+          dut_ff dut_n (.q(q[i]),.clk(~q[i-1]),.reset(reset));
 		end
 	endgenerate
 
 endmodule
 
 // Toggle Flip-Flop
-module tff (q, clk, reset);
+module dut_ff (q, clk, reset);
 
   output q;
   input clk, reset;
