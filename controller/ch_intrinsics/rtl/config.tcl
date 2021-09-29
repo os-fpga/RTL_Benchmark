35712c2f8fff2008b3b0d255a17ee1c4355ff6a8
​laraibkhan-rs <laraib.khan@rapidsilicon.com>
2021-09-29 15:59:22 +0500

    memset renamed

--- controller/ch_intrinsics/rtl/config.tcl
+++ controller/ch_intrinsics/rtl/config.tcl
@@ -1,5 +1,5 @@
 set ::env(PROJECT_NAME) ch_intrinsics
-set ::env(DESIGN_TOP) memory_controller_top
+set ::env(DESIGN_TOP) memset
 set ::env(DESIGN_DIR) RTL_Benchmark/controller/ch_intrinsics/rtl
 set ::env(TOP_VERILOG) RTL_Benchmark/controller/ch_intrinsics/rtl/ch_intrinsics_top.v
 set ::env(CLOCK_PORT) clk
