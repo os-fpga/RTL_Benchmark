b1e9d91b47087e3c02fc05f0e54b0da5f948cc99
​laraibkhan-rs <laraib.khan@rapidsilicon.com>
2021-10-04 10:38:30 +0500

    Clock added to counter's config

--- scalable_counters/down_counter/rtl/config.tcl
+++ scalable_counters/down_counter/rtl/config.tcl
@@ -2,4 +2,5 @@ set ::env(PROJECT_NAME) down_counter
 set ::env(DESIGN_TOP) param_down_counter
 set ::env(DESIGN_DIR) RTL_Benchmark/scalable_counters/down_counter/rtl
 set ::env(TOP_VERILOG) RTL_Benchmark/scalable_counters/down_counter/rtl/param_down_counter.v
+set ::env(CLOCK_PORT) clk_counter
 set ::env(DOMAIN) scalable_counters
