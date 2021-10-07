6a934a0bd646339462abb6c374b88923f860627e
Laraib Khan <87127798+laraibkhan-rs@users.noreply.github.com>
2021-10-07 16:49:17 +0500

    Update atahost_top.v

--- ocidec-2/atahost_top.v
+++ ocidec-2/atahost_top.v
@@ -65,7 +65,7 @@
 // CS1-		select control block registers
 //
 
-
+// top file
 module atahost_top (wb_clk_i, arst_i, wb_rst_i, wb_cyc_i, wb_stb_i, wb_ack_o, wb_err_o,
 		wb_adr_i, wb_dat_i, wb_dat_o, wb_sel_i, wb_we_i, wb_inta_o,
 		resetn_pad_o, dd_pad_i, dd_pad_o, dd_padoe_o, da_pad_o, cs0n_pad_o,
