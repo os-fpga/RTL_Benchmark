f8b9366623009195ac9b6ee8ce6ed09ed8bf1985
haseebahmad-rs <haseeb.ahmad@rapidsilicon.com>
2021-10-12 09:24:08 +0500

    corrected parameters in config

--- param_counter/config.tcl
+++ param_counter/config.tcl
@@ -2,5 +2,5 @@ set ::env(PROJECT_NAME) param_counter
 set ::env(DESIGN_TOP) param_up_counter
 set ::env(DESIGN_DIR) RTL_Benchmark/param_counter
 set ::env(TOP_VERILOG) RTL_Benchmark/param_up_counter.v
-set ::env(CLOCK_PORT) clk
-set ::env(DOMAIN) Lut_based
+set ::env(CLOCK_PORT) clk_counter
+set ::env(DOMAIN) lut_based
