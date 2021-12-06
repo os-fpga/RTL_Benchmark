945a26bada439965688635beafc96d7a7339623a
Laraib Khan <87127798+laraibkhan-rs@users.noreply.github.com>
2021-12-06 12:12:05 +0500

    Update simple_dual_one_clock.v
    name added as comment in design

--- micro_benchmarks/Xilinx_RAMs/simple_dual_one_clock.v
+++ micro_benchmarks/Xilinx_RAMs/simple_dual_one_clock.v
@@ -1,3 +1,4 @@
+////////////////////////// SIMPLE DUAL PORT ONE CLOCK RAM ///////////////////
 module simple_dual_one_clock (clk,ena,enb,wea,addra,addrb,dia,dob);
 input clk,ena,enb,wea;
 input [9:0] addra,addrb;
