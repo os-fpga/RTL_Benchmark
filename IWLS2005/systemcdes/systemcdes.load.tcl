d75d9a052f6e09dd50cd714dc748e7c78f8b413c
abdulhameed-rs <abdulhameed.akram@rapidsilicon.com>
2021-12-24 17:19:07 +0500

    81 designs added to the repo

---
+++ IWLS2005/systemcdes/systemcdes.load.tcl
@@ -0,0 +1,13 @@
+load { \
+../rtl/systemcdes/desround.v \
+../rtl/systemcdes/des.v \
+../rtl/systemcdes/key_gen.v \
+../rtl/systemcdes/s1.v \
+../rtl/systemcdes/s2.v \
+../rtl/systemcdes/s3.v \
+../rtl/systemcdes/s4.v \
+../rtl/systemcdes/s5.v \
+../rtl/systemcdes/s6.v \
+../rtl/systemcdes/s7.v \
+../rtl/systemcdes/s8.v \
+}
