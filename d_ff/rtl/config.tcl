96e5668c2abf54fb0b457081ba62e4dd6aef1b2f
nadeemyaseen-rs <nadeem.yaseen@rapidsilicon.com>
2021-12-30 18:42:18 +0500

    corrected path in in config.tcl

--- d_ff/rtl/config.tcl
+++ d_ff/rtl/config.tcl
@@ -1,6 +1,6 @@
 set ::env(PROJECT_NAME) d_ff
 set ::env(DESIGN_TOP) d_ff
-set ::env(DESIGN_DIR) .github/ci_unit_tests/d_ff/rtl
-set ::env(TOP_VERILOG) .github/ci_unit_tests/d_ff/rtl/d_ff.v
+set ::env(DESIGN_DIR) RTL_Benchmark/d_ff/rtl
+set ::env(TOP_VERILOG) RTL_Benchmark/d_ff/rtl/d_ff.v
 set ::env(CLOCK_PORT) clk
 set ::env(DOMAIN) ci_unit_tests
