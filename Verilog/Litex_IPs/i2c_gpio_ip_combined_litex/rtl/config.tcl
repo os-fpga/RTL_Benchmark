138b32fad4587b07322d2b3b39e05aa5f7c69631
Awais Abbas <awais.abbas@rapidsilicon.com>
2022-02-22 17:57:54 +0500

    Designs generated from Litex tool are added to RTL_Benchmarks

---
+++ Verilog/Litex_IPs/i2c_gpio_ip_combined_litex/rtl/config.tcl
@@ -0,0 +1,6 @@
+set ::env(PROJECT_NAME) i2c_gpio_ip_combined_litex
+set ::env(DESIGN_TOP) sim
+set ::env(DESIGN_DIR) RTL_Benchmark/Verilog/Litex_IPs/i2c_gpio_ip_combined_litex/rtl
+set ::env(TOP_VERILOG) RTL_Benchmark/Verilog/Litex_IPs/i2c_gpio_ip_combined_litex/rtl/sim.v
+set ::env(CLOCK_COUNT) 1
+set ::env(DOMAIN) ip
