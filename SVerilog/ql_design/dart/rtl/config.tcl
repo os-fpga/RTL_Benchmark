cc5fbce17822aae9cd3815d2786bebae3ed53aec
Awais Abbas <awais.abbas@rapidsilicon.com>
2022-02-22 18:17:32 +0500

    System Verilog Benchmarks are added to RTL-Benchmarks

---
+++ SVerilog/ql_design/dart/rtl/config.tcl
@@ -0,0 +1,6 @@
+set ::env(PROJECT_NAME) dart
+set ::env(DESIGN_TOP) dart
+set ::env(DESIGN_DIR) RTL_Benchmark/SVerilog/ql_design/dart/rtl/
+set ::env(TOP_VERILOG) RTL_Benchmark/SVerilog/ql_design/dart/rtl/dart.v
+set ::env(CLOCK_COUNT) 2
+set ::env(DOMAIN) digital_design
