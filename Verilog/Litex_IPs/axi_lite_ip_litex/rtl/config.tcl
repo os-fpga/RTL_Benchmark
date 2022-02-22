c480f2bbe3623a1a4b07f5648561718d37b8e307
Awais Abbas <awais.abbas@rapidsilicon.com>
2022-02-22 17:57:54 +0500

    Designs generated from Litex tool are added to RTL_Benchmarks

---
+++ Verilog/Litex_IPs/axi_lite_ip_litex/rtl/config.tcl
@@ -0,0 +1,6 @@
+set ::env(PROJECT_NAME) axi_lite_ip_litex
+set ::env(DESIGN_TOP) sim
+set ::env(DESIGN_DIR) RTL_Benchmark/Verilog/Litex_IPs/axi_lite_ip_litex/rtl
+set ::env(TOP_VERILOG) RTL_Benchmark/Verilog/Litex_IPs/axi_lite_ip_litex/rtl/sim.v
+set ::env(CLOCK_COUNT) 1
+set ::env(DOMAIN) ip
---
+++ Verilog/Litex_IPs/uart_ip_litex/rtl/config.tcl
@@ -0,0 +1,6 @@
+set ::env(PROJECT_NAME) uart_ip_litex
+set ::env(DESIGN_TOP) sim
+set ::env(DESIGN_DIR) RTL_Benchmark/Verilog/Litex_IPs/uart_ip_litex/rtl
+set ::env(TOP_VERILOG) RTL_Benchmark/Verilog/Litex_IPs/uart_ip_litex/rtl/sim.v
+set ::env(CLOCK_COUNT) 1
+set ::env(DOMAIN) ip
