5f032001a3095bb05bbf884ce42f6905946cae05
Awais Abbas <awais.abbas@rapidsilicon.com>
2022-02-22 17:46:22 +0500

    Old files deleted

--- param_adder/config.tcl
+++
@@ -1,5 +0,0 @@
-set ::env(PROJECT_NAME) param_adder
-set ::env(DESIGN_TOP) param_adder
-set ::env(DESIGN_DIR) RTL_Benchmark/param_adder
-set ::env(TOP_VERILOG) RTL_Benchmark/param_adder.v
-set ::env(DOMAIN) lut_based
--- param_counter/config.tcl
+++
@@ -1,6 +0,0 @@
-set ::env(PROJECT_NAME) param_counter
-set ::env(DESIGN_TOP) param_up_counter
-set ::env(DESIGN_DIR) RTL_Benchmark/param_counter
-set ::env(TOP_VERILOG) RTL_Benchmark/param_up_counter.v
-set ::env(CLOCK_PORT) clk_counter
-set ::env(DOMAIN) lut_based
