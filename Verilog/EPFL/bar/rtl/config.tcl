63b5a3331ff066af7ec0e4c635acf370553991e5
Awais Abbas <awais.abbas@rapidsilicon.com>
2022-02-22 17:50:52 +0500

    Design from EPFL are added to RTL_Benchmarks

---
+++ Verilog/EPFL/bar/rtl/config.tcl
@@ -0,0 +1,6 @@
+set ::env(PROJECT_NAME) bar
+set ::env(DESIGN_TOP) top
+set ::env(DESIGN_DIR) RTL_Benchmark/Verilog/EPFL/bar/rtl
+set ::env(TOP_VERILOG) RTL_Benchmark/Verilog/EPFL/bar/rtl/bar.v
+set ::env(CLOCK_COUNT) 0
+set ::env(DOMAIN) EPFL
---
+++ Verilog/EPFL/i2c/rtl/config.tcl
@@ -0,0 +1,6 @@
+set ::env(PROJECT_NAME) i2c
+set ::env(DESIGN_TOP) i2c
+set ::env(DESIGN_DIR) RTL_Benchmark/Verilog/EPFL/i2c/rtl
+set ::env(TOP_VERILOG) RTL_Benchmark/Verilog/EPFL/i2c/rtl/i2c.v
+set ::env(CLOCK_COUNT) 0
+set ::env(DOMAIN) EPFL
