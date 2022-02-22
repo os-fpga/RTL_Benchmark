e115a858a69d522778a75e42e1c10ff2e045849c
Awais Abbas <awais.abbas@rapidsilicon.com>
2022-02-22 17:46:22 +0500

    Old files deleted

--- des_area_opt/config.tcl
+++
@@ -1,6 +0,0 @@
-set ::env(PROJECT_NAME) des_ao
-set ::env(DESIGN_TOP) des_top
-set ::env(DESIGN_DIR) RTL_Benchmark/des_area_opt
-set ::env(TOP_VERILOG) RTL_Benchmark/des_area_opt/des_top.v
-set ::env(CLOCK_PORT) clk
-set ::env(DOMAIN) encryption
--- ocidec-1/config.tcl
+++
@@ -1,6 +0,0 @@
-set ::env(PROJECT_NAME) ata_ocidec-1
-set ::env(DESIGN_TOP) atahost_top
-set ::env(DESIGN_DIR) RTL_Benchmark/ocidec-1
-set ::env(TOP_VERILOG) RTL_Benchmark/ocidec-1/atahost_top.v
-set ::env(CLOCK_PORT) wb_clk_i
-set ::env(DOMAIN) controller
