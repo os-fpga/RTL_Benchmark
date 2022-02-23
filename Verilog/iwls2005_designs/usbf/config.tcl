c3956295f6baaba64745cc06df4d2e18fd8ffbac
Awais Abbas <awais.abbas@rapidsilicon.com>
2022-02-23 13:13:37 +0500

    Missing IWLS2005 design added

---
+++ Verilog/iwls2005_designs/usbf/config.tcl
@@ -0,0 +1,6 @@
+set ::env(PROJECT_NAME) usbf_top
+set ::env(DESIGN_TOP) usbf_top
+set ::env(DESIGN_DIR) RTL_Benchmark/Verilog/iwls2005_designs/usbf/
+set ::env(TOP_VERILOG) RTL_Benchmark/Verilog/iwls2005_designs/usbf/usbf_top.v
+set ::env(CLOCK_COUNT) 2
+set ::env(DOMAIN) iwls2005_designs
