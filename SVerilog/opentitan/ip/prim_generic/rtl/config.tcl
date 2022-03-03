3514f6fd63ef9b6fcc5bb350014d13fc2b450ae0
Komal Inayat <komal.inayat@rapidsilicon.com>
2022-03-03 10:13:18 +0500

    New deisgns are added

---
+++ SVerilog/opentitan/ip/prim_generic/rtl/config.tcl
@@ -0,0 +1,7 @@
+ 
+set ::env(PROJECT_NAME) orim_generic
+set ::env(DESIGN_TOP) prim_generic_flash
+set ::env(DESIGN_DIR) New_RTL/opentitan/IP/ip/prim_generic/rtl
+set ::env(TOP_VERILOG) New_RTL/opentitan/IP/ip/prim_generic/rtl/prim_generic_flash.sv
+set ::env(CLOCK_COUNT) 1
+set ::env(DOMAIN) digital_design
