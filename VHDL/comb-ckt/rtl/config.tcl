72edfea6756d18c899bdb72560ab8ebc72983d71
zaheerahmad-rs <zaheer.ahmad@rapidsilicon.com>
2022-03-11 16:14:24 +0500

    Config.json and benchmark.json updated for VHDL designs

---
+++ VHDL/comb-ckt/rtl/config.tcl
@@ -0,0 +1,6 @@
+set ::env(PROJECT_NAME) comb-ckt
+set ::env(DESIGN_TOP) comb_ckt
+set ::env(DESIGN_DIR) RTL_Benchmark/VHDL/comb-ckt/rtl
+set ::env(TOP_VERILOG) RTL_Benchmark/VHDL/comb-ckt/rtl/comb_ckt.vhd
+set ::env(CLOCK_COUNT) 0
+set ::env(DOMAIN) OpenCores_designs
