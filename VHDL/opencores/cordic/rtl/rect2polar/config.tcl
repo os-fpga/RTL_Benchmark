19fe24ee8d63bd80d77566353e9d5dbe5bcc56ad
zaheerahmad-rs <zaheer.ahmad@rapidsilicon.com>
2022-03-14 12:40:25 +0500

    Updated config.json and benchmark.json for all VHDL designs

---
+++ VHDL/opencores/cordic/rtl/rect2polar/config.tcl
@@ -0,0 +1,6 @@
+set ::env(PROJECT_NAME) rect2polar
+set ::env(DESIGN_TOP) r2p_corproc
+set ::env(DESIGN_DIR) RTL_Benchmark/VHDL/opencores/cordic/rtl/rect2polar
+set ::env(TOP_VERILOG) RTL_Benchmark/VHDL/opencores/cordic/rtl/rect2polar/r2p_corproc.vhd
+set ::env(CLOCK_COUNT) 1
+set ::env(DOMAIN) OpenCores_designs
---
+++ VHDL/opencores/sdc_controller/rtl/verilog/config.tcl
@@ -0,0 +1,6 @@
+set ::env(PROJECT_NAME) sdc_controller
+set ::env(DESIGN_TOP) sdc_controller_top
+set ::env(DESIGN_DIR) RTL_Benchmark/controller/sdc_controller/rtl/verilog
+set ::env(TOP_VERILOG) RTL_Benchmark/controller/sdc_controller/rtl/verilog/sdc_controller_top.v
+set ::env(CLOCK_COUNT) 3
+set ::env(DOMAIN) controller
