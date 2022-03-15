9fe39dcd708acb8fff7ef346e00623834c0f560a
zaheerahmad-rs <zaheer.ahmad@rapidsilicon.com>
2022-03-15 17:44:14 +0500

    added config.json for sv files

--- SVerilog/Cores/UDMA_core/rtl/config.tcl
+++ SVerilog/Cores/UDMA_core/rtl/config.tcl
@@ -1,6 +1,6 @@
-set ::env(PROJECT_NAME) udma_core
+set ::env(PROJECT_NAME) UDMA_core
 set ::env(DESIGN_TOP) udma_core
-set ::env(DESIGN_DIR) RTL_Benchmark/core/udma_core/rtl
-set ::env(TOP_VERILOG) RTL_Benchmark/core/udma_core/rtl/udma_core.sv
+set ::env(DESIGN_DIR) RTL_Benchmark/SVerilog/Cores/UDMA_core/rtl
+set ::env(TOP_VERILOG) RTL_Benchmark/SVerilog/Cores/UDMA_core/rtl/udma_core.sv
 set ::env(CLOCK_COUNT) 2
 set ::env(DOMAIN) core
--- SVerilog/Cores/cva_64_a/rtl/config.tcl
+++ SVerilog/Cores/cva_64_a/rtl/config.tcl
@@ -1,6 +1,6 @@
 set ::env(PROJECT_NAME) cva_64_a
 set ::env(DESIGN_TOP) ariane
 set ::env(DESIGN_DIR) RTL_Benchmark/SVerilog/Cores/cva_64_a/rtl
-set ::env(TOP_VERILOG) RTL_Benchmark/SVerilog/Cores/cva_64_a/ariane.sv
+set ::env(TOP_VERILOG) RTL_Benchmark/SVerilog/Cores/cva_64_a/rtl/ariane.sv
 set ::env(CLOCK_COUNT) 1
 set ::env(DOMAIN) core
