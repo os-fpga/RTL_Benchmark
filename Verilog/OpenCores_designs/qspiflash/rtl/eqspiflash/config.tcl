bf682013379178d2acffab5482cbb74543f60a44
Yasir Farooq <yasir.farooq@ithelp@lampromellon.com>
2022-03-16 18:04:28 +0500

    Added Verilog design for Communication Controller domain

---
+++ Verilog/OpenCores_designs/qspiflash/rtl/eqspiflash/config.tcl
@@ -0,0 +1,6 @@
+set ::env(PROJECT_NAME) eqspiflash
+set ::env(DESIGN_TOP) eqspiflash
+set ::env(DESIGN_DIR) RTL_Benchmark/Verilog/OpenCores_designs/qspiflash/rtl/eqspiflash
+set ::env(TOP_VERILOG) RTL_Benchmark/Verilog/OpenCores_designs/qspiflash/rtl/eqspiflash/eqspiflash.v
+set ::env(CLOCK_COUNT) 1
+set ::env(DOMAIN) Communication
---
+++ Verilog/OpenCores_designs/qspiflash/rtl/wbqspiflash/config.tcl
@@ -0,0 +1,6 @@
+set ::env(PROJECT_NAME) wbqspiflash
+set ::env(DESIGN_TOP) wbqspiflash
+set ::env(DESIGN_DIR) RTL_Benchmark/Verilog/OpenCores_designs/qspiflash/rtl/wbqspiflash
+set ::env(TOP_VERILOG) RTL_Benchmark/Verilog/OpenCores_designs/qspiflash/rtl/wbqspiflash/wbqspiflash.v
+set ::env(CLOCK_COUNT) 1
+set ::env(DOMAIN) Communication
