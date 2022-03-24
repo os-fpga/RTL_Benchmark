c6dab1bf8b0a438560c8364ac81a77d88645bbf3
Komal Inayat <komal.inayat@rapidsilicon.com>
2022-03-24 17:34:32 +0500

    Unnecessary files are removed

---
+++ SVerilog/Cores/Cores-SweRV-EH2/design/exu/config.tcl
@@ -0,0 +1,9 @@
+ 
+set ::env(PROJECT_NAME) eh2_exu
+set ::env(DESIGN_TOP) eh2_exu
+set ::env(DESIGN_DIR) RTL_Benchmark/SVerilog/Chipalliance/Cores-SweRV-EH2/design/exu
+set ::env(TOP_VERILOG) RTL_Benchmark/SVerilog/Chipalliance/Cores-SweRV-EH2/design/exu/eh2_exu.sv
+set ::env(CLOCK_COUNT) 3
+set ::env(DOMAIN) digital_design
+
+
