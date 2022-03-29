bf700f4ae127012aec3f2aa110e5e5f0a5e62a09
Komal Inayat <komal.inayat@rapidsilicon.com>
2022-03-29 10:31:54 +0500

    Cores_SweRV-EL2 added

---
+++ SVerilog/Cores/Cores-SweRV-EL2/design/dec/config.tcl
@@ -0,0 +1,7 @@
+ 
+set ::env(PROJECT_NAME) dec
+set ::env(DESIGN_TOP) el2_dec
+set ::env(DESIGN_DIR) RTL_Benchmark/SVerilog/Chipalliance/Cores-SweRV-EL2/design/dec
+set ::env(TOP_VERILOG) RTL_Benchmark/SVerilog/Chipalliance/Cores-SweRV-EL2/design/dec/el2_dec.sv
+set ::env(CLOCK_COUNT) 4
+set ::env(DOMAIN) digital_design
---
+++ SVerilog/Cores/Cores-SweRV-EL2/design/lib/ahb_to_axi4/config.tcl
@@ -0,0 +1,11 @@
+ 
+set ::env(PROJECT_NAME) ahb_to_axi4
+set ::env(DESIGN_TOP) ahb_to_axi4
+set ::env(DESIGN_DIR) RTL_Benchmark/SVerilog/Chipalliance/Cores-SweRV-EL2/design/lib/ahb_to_axi4
+set ::env(TOP_VERILOG) RTL_Benchmark/SVerilog/Chipalliance/Cores-SweRV-EL2/design/lib/ahb_to_axi4/ahb_to_axi4.sv
+set ::env(CLOCK_COUNT) 3
+set ::env(DOMAIN) digital_design
+
+
+
+
