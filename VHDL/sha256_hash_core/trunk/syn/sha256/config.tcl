c1ace735d8b772107a52195d8f4a484307c92a49
Komal Inayat <komal.inayat@rapidsilicon.com>
2022-04-07 17:08:54 +0500

    Crypto_Core_Design's Config.TCL Files added

---
+++ VHDL/sha256_hash_core/trunk/syn/sha256/config.tcl
@@ -0,0 +1,9 @@
+ 
+set ::env(PROJECT_NAME) systemcdes
+set ::env(DESIGN_TOP) wb_descontroller
+set ::env(DESIGN_DIR)  RTL_Benchmark/other/systemcdes/trunk/rtl/verilog
+set ::env(TOP_VERILOG) RTL_Benchmark/other/systemcdes/trunk/rtl/verilog/wb_descontroller.v
+set ::env(CLOCK_COUNT) 1
+set ::env(DOMAIN) digital_design
+
+file:///home/users/komal.inayat/Desktop/Gap-Analysis/RTL_Benchmark/VHDL/sha256_hash_core/trunk/syn/sha256/gv_sha256.vhd
