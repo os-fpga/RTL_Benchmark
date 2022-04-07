9e9459c73ac99db8a666f5d9c557c3ef745a6b38
Ayyaz Ahmed <ayyaz.ahmed@rapidsilicon.com>
2022-04-07 11:14:19 +0500

    updated the rtl path in config file

--- SVerilog/opentitan/ip/flash_ctrl/rtl/config.tcl
+++ SVerilog/opentitan/ip/flash_ctrl/rtl/config.tcl
@@ -1,7 +1,7 @@
  
 set ::env(PROJECT_NAME) flash_ctrl
 set ::env(DESIGN_TOP) flash_ctrl
-set ::env(DESIGN_DIR) RTL_Benchmark/entropy_src/ip/flash_ctrl/rtl
-set ::env(TOP_VERILOG) RTL_Benchmark/entropy_src/ip/flash_ctrl/rtl/flash_ctrl.sv
+set ::env(DESIGN_DIR) RTL_Benchmark/SVerilog/opentitan/ip/flash_ctrl/rtl
+set ::env(TOP_VERILOG) RTL_Benchmark/SVerilog/opentitan/ip/flash_ctrl/rtl/flash_ctrl.sv
 set ::env(CLOCK_COUNT) 2
 set ::env(DOMAIN) digital_design
