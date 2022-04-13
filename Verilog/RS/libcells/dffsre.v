ca10dc84d73e4e53b638c4dcca89e7c35e3cd623
Awais Abbas <awais.abbas@rapidsilicon.com>
2022-04-13 13:20:04 +0500

    lib cells added

---
+++ Verilog/RS/libcells/dffsre.v
@@ -0,0 +1,105 @@
+module dff(
+    output reg Q,
+    input D,
+    (* clkbuf_sink *)
+    input C
+);
+    parameter [0:0] INIT = 1'b0;
+    initial Q = INIT;
+    always @(posedge C)
+        Q <= D;
+endmodule
+
+module dffn(
+    output reg Q,
+    input D,
+    (* clkbuf_sink *)
+    input C
+);
+    parameter [0:0] INIT = 1'b0;
+    initial Q = INIT;
+    always @(negedge C)
+        Q <= D;
+endmodule
+
+module dffsre(
+    output reg Q,
+    input D,
+    (* clkbuf_sink *)
+    input C,
+    input E,
+    input R,
+    input S
+);
+    parameter [0:0] INIT = 1'b0;
+    initial Q = INIT;
+
+    always @(posedge C or negedge S or negedge R)
+        if (!R)
+            Q <= 1'b0;
+        else if (!S)
+            Q <= 1'b1;
+        else if (E)
+            Q <= D;
+        
+endmodule
+
+module dffnsre(
+    output reg Q,
+    input D,
+    (* clkbuf_sink *)
+    input C,
+    input E,
+    input R,
+    input S
+);
+    parameter [0:0] INIT = 1'b0;
+    initial Q = INIT;
+
+    always @(negedge C or negedge S or negedge R)
+        if (!R)
+            Q <= 1'b0;
+        else if (!S)
+            Q <= 1'b1;
+        else if (E)
+            Q <= D;
+        
+endmodule
+
+module latchsre (
+    output reg Q,
+    input S,
+    input R,
+    input D,
+    input G,
+    input E
+);
+    parameter [0:0] INIT = 1'b0;
+    initial Q = INIT;
+    always @*
+        if (!R) 
+            Q <= 1'b0;
+        else if (!S) 
+            Q <= 1'b1;
+        else if (E && G) 
+            Q <= D;
+endmodule
+
+module latchnsre (
+    output reg Q,
+    input S,
+    input R,
+    input D,
+    input G,
+    input E
+);
+    parameter [0:0] INIT = 1'b0;
+    initial Q = INIT;
+    always @*
+        if (!R) 
+            Q <= 1'b0;
+        else if (!S) 
+            Q <= 1'b1;
+        else if (E && !G) 
+            Q <= D;
+endmodule
