84f4a11a5b40a17801e321e350e191ce01fa3e1f
zaheerahmad-rs <zaheer.ahmad@rapidsilicon.com>
2022-04-21 13:30:01 +0500

    Added random generated designs in random_test_cases

---
+++ Verilog/random_test_cases/design7/config.tcl
@@ -0,0 +1,6 @@
+set ::env(PROJECT_NAME) design7
+set ::env(DESIGN_TOP) design7_20_35_top
+set ::env(DESIGN_DIR) RTL_Benchmark/Verilog/random_test_cases/design7
+set ::env(TOP_VERILOG) RTL_Benchmark/Verilog/random_test_cases/design7/design7_20_35.v
+set ::env(CLOCK_COUNT) 1
+set ::env(DOMAIN) random_design_gen
---
+++ Verilog/random_test_cases/design8/config.tcl
@@ -0,0 +1,6 @@
+set ::env(PROJECT_NAME) design8
+set ::env(DESIGN_TOP) design8_10_30_top
+set ::env(DESIGN_DIR) RTL_Benchmark/Verilog/random_test_cases/design8
+set ::env(TOP_VERILOG) RTL_Benchmark/Verilog/random_test_cases/design8/design8_10_30.v
+set ::env(CLOCK_COUNT) 1
+set ::env(DOMAIN) random_design_gen
