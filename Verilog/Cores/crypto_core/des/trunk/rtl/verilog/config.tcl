2591f66b7ae49d4a92621fefcde7096c0161ad0e
Komal Inayat-RS <95079715+komalinayat@users.noreply.github.com>
2022-04-27 15:09:00 +0500

    Update config.tcl

--- Verilog/Cores/crypto_core/des/trunk/rtl/verilog/config.tcl
+++ Verilog/Cores/crypto_core/des/trunk/rtl/verilog/config.tcl
@@ -1,5 +1,5 @@
  
-set ::env(PROJECT_NAME) des
+set ::env(PROJECT_NAME) des3_crypto
 set ::env(DESIGN_TOP) des3
 set ::env(DESIGN_DIR)  RTL_Benchmark/Verilog/Cores/crypto_core/des/trunk/rtl/verilog
 set ::env(TOP_VERILOG) RTL_Benchmark/Verilog/Cores/crypto_core/des/trunk/rtl/verilog/des3.v
