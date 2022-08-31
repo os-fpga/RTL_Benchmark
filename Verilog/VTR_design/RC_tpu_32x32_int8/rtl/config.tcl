74ad557785d1ccfa85ec330490421774e372c5c1
komalinayat <komal.inayat@rapidsilicon.com>
2022-08-31 13:26:05 +0500

    RC_tpu_32x32_int8 file updated

---
+++ Verilog/VTR_design/RC_tpu_32x32_int8/rtl/config.tcl
@@ -0,0 +1,6 @@
+set ::env(PROJECT_NAME) RC_tpu.32x32.int8
+set ::env(DESIGN_TOP) matmul_32x32_systolic
+set ::env(DESIGN_DIR) RTL_Benchmark/Verilog/VTR_design/RC_tpu.32x32_int8/rtl
+set ::env(TOP_VERILOG) RTL_Benchmark/Verilog/VTR_design/RC_tpu.32x3int8/rtl/tpu.32x32.int8.v
+set ::env(CLOCK_COUNT) 1
+set ::env(DOMAIN) VTR_design
