224594b86fd373e80421c6a0e7a15f27b6409ee4
Lia Vardanyan <liav@rapidsilicon.com>
2022-10-10 12:25:40 +0500

    Added EPFL benchmarks (Verilog and VHDL).

---
+++ VHDL/EPFL/log2/rtl/config.tcl
@@ -0,0 +1,6 @@
+set ::env(PROJECT_NAME) log2
+set ::env(DESIGN_TOP) top
+set ::env(DESIGN_DIR) RTL_Benchmark/VHDL/EPFL/log2/rtl
+set ::env(TOP_VERILOG) RTL_Benchmark/VHDL/EPFL/log2/rtl/log2.vhd
+set ::env(CLOCK_COUNT) 0
+set ::env(DOMAIN) EPFL
