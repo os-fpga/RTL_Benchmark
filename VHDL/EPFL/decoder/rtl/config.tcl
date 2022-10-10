86e9e29f97c8989408e5b96b2de17df172d3d16b
Lia Vardanyan <liav@rapidsilicon.com>
2022-10-10 12:25:40 +0500

    Added EPFL benchmarks (Verilog and VHDL).

---
+++ VHDL/EPFL/decoder/rtl/config.tcl
@@ -0,0 +1,6 @@
+set ::env(PROJECT_NAME) decoder
+set ::env(DESIGN_TOP) dec
+set ::env(DESIGN_DIR) RTL_Benchmark/VHDL/EPFL/decoder/rtl
+set ::env(TOP_VERILOG) RTL_Benchmark/VHDL/EPFL/decoder/rtl/dec.vhd
+set ::env(CLOCK_COUNT) 0
+set ::env(DOMAIN) EPFL
---
+++ Verilog/EPFL/alu_control_unit/rtl/config.tcl
@@ -0,0 +1,6 @@
+set ::env(PROJECT_NAME) alu_control_unit
+set ::env(DESIGN_TOP) top
+set ::env(DESIGN_DIR) RTL_Benchmark/Verilog/EPFL/alu_control_unit/rtl
+set ::env(TOP_VERILOG) RTL_Benchmark/Verilog/EPFL/alu_control_unit/rtl/ctrl.v
+set ::env(CLOCK_COUNT) 0
+set ::env(DOMAIN) EPFL
