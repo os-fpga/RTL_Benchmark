bbbc3ac139f31ead063e3ef27ceead59cbea2f90
Awais Abbas <awais.abbas@rapidsilicon.com>
2023-04-19 14:42:08 +0500

    unnecessasy files deleted

--- Verilog/yosys_validation/EDA-1279/dsp_mul_signed_reg_active_low_async_reset/raptor_tcl.tcl
+++
@@ -1,8 +0,0 @@
-create_design dsp_mul_signed_reg_active_low_async_reset
-target_device GEMINI
-add_include_path ./rtl
-add_library_path ./rtl
-add_library_ext .v .sv
-add_design_file ./rtl/dsp_mul_signed_reg_active_low_async_reset.v
-set_top_module dsp_mul_signed_reg_active_low_async_reset
-synthesize delay
