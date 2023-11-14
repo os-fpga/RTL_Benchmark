6ec9b86b3779ba9f28e9d4daf3a1772f051dd0d2
komalinayat <komal.inayat@rapidsilicon.com>
2023-11-14 18:12:31 +0500

    Extra Designs are removed from Repo

--- Verilog/OpenCores_designs/Communication_Controller/auto_baud/trunk/auto_baud_with_tracking/config.tcl
+++
@@ -1,7 +0,0 @@
-set ::env(PROJECT_NAME) auto_baud_with_tracking
-set ::env(DESIGN_TOP) auto_baud_with_tracking
-set ::env(DESIGN_DIR) RTL_Benchmark/Verilog/OpenCores_designs/Communication_Controller/auto_baud/trunk/auto_baud_with_tracking
-set ::env(TOP_VERILOG) RTL_Benchmark/Verilog/OpenCores_designs/Communication_Controller/auto_baud/trunk/auto_baud_with_tracking/auto_baud_with_tracking.v
-set ::env(CLOCK_COUNT) 2
-set ::env(DOMAIN) controller
-
